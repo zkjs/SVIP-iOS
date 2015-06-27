3862fa98102f681d61235238f10708834755bfa2
Hanton <hanton@zkjinshi.com>
2015-06-27 14:32:53 +0800

    Add Objective-C Support

--- SVIP/AppDelegate.swift
+++ SVIP/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.rootViewController = ViewController()
+    window?.rootViewController?.view.layer.cornerRadius = 6
+    window?.rootViewController?.view.layer.masksToBounds = true
+    window?.makeKeyAndVisible()
     return true
   }
 
