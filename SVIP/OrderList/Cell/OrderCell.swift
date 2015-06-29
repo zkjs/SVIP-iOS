e14bc5c049b8724f5f3974e9ec75d785c31cf985
Hanton <hanton@zkjinshi.com>
2015-06-29 15:20:11 +0800

    大概的程序框架

---
+++ SVIP/OrderList/Cell/OrderCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderCell.swift
+//  SVIP
+//
+//  Created by Hanton on 6/29/15.
+//  Copyright (c) 2015 zkjinshi. All rights reserved.
+//
+
+import UIKit
+
+class OrderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
