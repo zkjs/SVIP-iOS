251e2d21f0e8091567e7c8b075f69bda821147a9
Hanton <hanton@zkjinshi.com>
2015-08-27 18:08:43 +0800

    确定订单页面-暂时性

--- SVIP/Book/Controller/BookingOrderDetailTVC.swift
+++ SVIP/Book/Controller/BookingOrderDetailTVC.swift
@@ -11,6 +11,10 @@ import UIKit
 private let kNameSection = 2
 private let kReceiptSection = 4
 private let kReceiptRow = 1
+private let kRoomSection = 5
+private let kRoomRow = 1
+private let kServiceSection = 6
+private let kServiceRow = 1
 private let partner = PartnerID
 private let seller = SellerID
 private let privateKey = PartnerPrivKey
@@ -26,9 +30,16 @@ class BookingOrderDetailTVC: UITableViewController, UITextFieldDelegate {
   @IBOutlet weak var receiptLabel: UILabel!
   @IBOutlet weak var remarkTextView: UITextView!
   
+  let roomTagView = SKTagView()
+  let serviceTagView = SKTagView()
+  
   var roomCount = 1
   var shopID = ""
   var bkOrder: BookOrder!
+  var roomTags = ["无烟房", "加床", "开夜床", "高楼层", "角落房", "安静", "离电梯近", "视野好", "数字敏感"]
+  var chosenRoomTags = [String]()
+  var serviceTags = ["免前台"]
+  var chosenServiceTags = [String]()
   
   // MARK: - View Lifecycle
   
@@ -164,6 +175,100 @@ class BookingOrderDetailTVC: UITableViewController, UITextFieldDelegate {
       count = roomCount
     }
     return count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = super.tableView(tableView, cellForRowAtIndexPath: indexPath)
+    
+    if indexPath.section == kRoomSection && indexPath.row == kRoomRow {
+//      let roomTagView = SKTagView()
+      roomTagView.backgroundColor = UIColor.whiteColor()
+      roomTagView.setTranslatesAutoresizingMaskIntoConstraints(false)
+      roomTagView.padding = UIEdgeInsetsMake(0.0, 15.0, 0.0, 15.0)
+      roomTagView.insets = 12.0
+      roomTagView.lineSpace = 12
+      roomTagView.didClickTagAtIndex = { [unowned self] (indexU: UInt) -> () in
+        let index = Int(indexU)
+        let clickedTag = self.roomTags[index]
+        if let chosenIndex = find(self.chosenRoomTags, clickedTag) {
+          let tagButton = self.roomTagView.tags[index] as! SKTag
+          tagButton.textColor = UIColor.blackColor()
+          tagButton.bgColor = UIColor.whiteColor()
+          self.chosenRoomTags.removeAtIndex(chosenIndex)
+        }  else {
+          let tagButton = self.roomTagView.tags[index] as! SKTag
+          tagButton.textColor = UIColor.whiteColor()
+          tagButton.bgColor = UIColor.blackColor()
+          self.chosenRoomTags.append(self.roomTags[index])
+        }
+        println(self.chosenRoomTags)
+      }
+      cell.contentView.addSubview(roomTagView)
+      roomTagView.mas_makeConstraints { (make: MASConstraintMaker!) -> Void in
+        let superView = cell.contentView
+        make.top.equalTo()(superView.mas_top)
+        make.bottom.equalTo()(superView.mas_bottom)
+        make.leading.equalTo()(superView.mas_leading)
+        make.trailing.equalTo()(superView.mas_trailing)
+      }
+      
+      for tag in roomTags {
+        let tagButton = SKTag(text: tag)
+        tagButton.textColor = UIColor.blackColor()
+        tagButton.fontSize = 17
+        tagButton.padding = UIEdgeInsetsMake(5.0, 12.0, 5.0, 12.0)
+        tagButton.bgColor = UIColor.whiteColor()
+        tagButton.borderColor = UIColor.grayColor()
+        tagButton.borderWidth = 0.5
+        tagButton.cornerRadius = 12.0
+        roomTagView.addTag(tagButton)
+      }
+    } else if indexPath.section == kServiceSection && indexPath.row == kServiceRow {
+//      let serviceTagView = SKTagView()
+      serviceTagView.backgroundColor = UIColor.whiteColor()
+      serviceTagView.setTranslatesAutoresizingMaskIntoConstraints(false)
+      serviceTagView.padding = UIEdgeInsetsMake(0.0, 15.0, 0.0, 15.0)
+      serviceTagView.insets = 12.0
+//      serviceTagView.lineSpace = 10
+      serviceTagView.didClickTagAtIndex = { [unowned self] (indexU: UInt) -> () in
+        let index = Int(indexU)
+        let clickedTag = self.serviceTags[index]
+        if let chosenIndex = find(self.chosenServiceTags, clickedTag) {
+          let tagButton = self.serviceTagView.tags[index] as! SKTag
+          tagButton.textColor = UIColor.blackColor()
+          tagButton.bgColor = UIColor.whiteColor()
+          self.chosenServiceTags.removeAtIndex(chosenIndex)
+        }  else {
+          let tagButton = self.serviceTagView.tags[index] as! SKTag
+          tagButton.textColor = UIColor.whiteColor()
+          tagButton.bgColor = UIColor.blackColor()
+          self.chosenServiceTags.append(self.serviceTags[index])
+        }
+        println(self.chosenServiceTags)
+      }
+      cell.contentView.addSubview(serviceTagView)
+      serviceTagView.mas_makeConstraints { (make: MASConstraintMaker!) -> Void in
+        let superView = cell.contentView
+        make.top.equalTo()(superView.mas_top)
+        make.bottom.equalTo()(superView.mas_bottom)
+        make.leading.equalTo()(superView.mas_leading)
+        make.trailing.equalTo()(superView.mas_trailing)
+      }
+      
+      for tag in serviceTags {
+        let tagButton = SKTag(text: tag)
+        tagButton.textColor = UIColor.blackColor()
+        tagButton.fontSize = 17
+        tagButton.padding = UIEdgeInsetsMake(12.0, 12.0, 12.0, 12.0)
+        tagButton.bgColor = UIColor.whiteColor()
+        tagButton.borderColor = UIColor.grayColor()
+        tagButton.borderWidth = 0.5
+        tagButton.cornerRadius = 12.0
+        serviceTagView.addTag(tagButton)
+      }
+    }
+    
+    return cell
   }
   
   // MARK: - Table view delegate
