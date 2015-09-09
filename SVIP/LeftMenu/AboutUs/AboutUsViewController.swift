5c4536573f76e8be8e7c5b3f29d40ad85aab1974
dai.fengyi <fengji@zkjinshi.com>
2015-09-09 21:10:38 +0800

    SkipCheckinSettingViewController

--- SVIP/LeftMenu/AboutUs/AboutUsViewController.swift
+++ SVIP/LeftMenu/AboutUs/AboutUsViewController.swift
@@ -9,44 +9,44 @@
 import UIKit
 
 class AboutUsViewController: UIViewController, UIWebViewDelegate {
-  @IBOutlet weak var switchStatus: UILabel!
-  @IBOutlet weak var switchButton: UISwitch!
-  var webView: UIWebView!
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    initSubviews()
-  }
-  override func viewWillDisappear(animated: Bool) {
-    super.viewWillDisappear(animated)
-    ZKJSTool.hideHUD()
-  }
-  func initSubviews() {
-    let v = NSBundle.mainBundle().loadNibNamed("SwitchView", owner: self, options: nil).last as! UIView
-    let right = UIBarButtonItem(customView:v)
-    self.navigationItem.rightBarButtonItem = right
-    
-    switchButton.addTarget(self, action:"switchValueChange:", forControlEvents: UIControlEvents.ValueChanged)
-    
-    webView = UIWebView(frame: view.bounds)
-    let url = NSURL(string: "http://iwxy.cc/mqt")
-    webView.loadRequest(NSURLRequest(URL: url!))
-    webView.scrollView.bounces = false
-    webView.delegate = self
-    view.addSubview(webView)
-  }
-  
-  func switchValueChange(sender: UISwitch) {
-    if switchButton.on {
-      switchStatus.text = "开启"
-    }else {
-      switchStatus.text = "关闭"
-    }
-  }
-  
-  func webViewDidStartLoad(webView: UIWebView) {
-    ZKJSTool.showLoading()
-  }
-  func webViewDidFinishLoad(webView: UIWebView) {
-    ZKJSTool.hideHUD()
-  }
+//  @IBOutlet weak var switchStatus: UILabel!
+//  @IBOutlet weak var switchButton: UISwitch!
+//  var webView: UIWebView!
+//  override func viewDidLoad() {
+//    super.viewDidLoad()
+//    initSubviews()
+//  }
+//  override func viewWillDisappear(animated: Bool) {
+//    super.viewWillDisappear(animated)
+//    ZKJSTool.hideHUD()
+//  }
+//  func initSubviews() {
+//    let v = NSBundle.mainBundle().loadNibNamed("SwitchView", owner: self, options: nil).last as! UIView
+//    let right = UIBarButtonItem(customView:v)
+//    self.navigationItem.rightBarButtonItem = right
+//    
+//    switchButton.addTarget(self, action:"switchValueChange:", forControlEvents: UIControlEvents.ValueChanged)
+//    
+//    webView = UIWebView(frame: view.bounds)
+//    let url = NSURL(string: "http://iwxy.cc/mqt")
+//    webView.loadRequest(NSURLRequest(URL: url!))
+//    webView.scrollView.bounces = false
+//    webView.delegate = self
+//    view.addSubview(webView)
+//  }
+//  
+//  func switchValueChange(sender: UISwitch) {
+//    if switchButton.on {
+//      switchStatus.text = "开启"
+//    }else {
+//      switchStatus.text = "关闭"
+//    }
+//  }
+//  
+//  func webViewDidStartLoad(webView: UIWebView) {
+//    ZKJSTool.showLoading()
+//  }
+//  func webViewDidFinishLoad(webView: UIWebView) {
+//    ZKJSTool.hideHUD()
+//  }
 }
--- SVIP/LeftMenu/SkipCheckIn/SkipCheckInSettingViewController.swift
+++ SVIP/LeftMenu/SkipCheckIn/SkipCheckInSettingViewController.swift
@@ -8,44 +8,45 @@
 
 import UIKit
 
-class SkipCheckInSettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  let Identifier = "Identifier"
-  @IBOutlet weak var tableView: UITableView!
-  
-  required override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-    super.init(nibName:"SkipCheckInSettingViewController", bundle: nil)
-  }
-  
-  required init(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-  }
- override func viewDidLoad() {
+class SkipCheckInSettingViewController: UIViewController, UIWebViewDelegate {
+  @IBOutlet weak var switchStatus: UILabel!
+  @IBOutlet weak var switchButton: UISwitch!
+  var webView: UIWebView!
+  override func viewDidLoad() {
     super.viewDidLoad()
+    initSubviews()
+  }
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    ZKJSTool.hideHUD()
+  }
+  func initSubviews() {
+    let v = NSBundle.mainBundle().loadNibNamed("SwitchView", owner: self, options: nil).last as! UIView
+    let right = UIBarButtonItem(customView:v)
+    self.navigationItem.rightBarButtonItem = right
     
-    // Do any additional setup after loading the view.
+    switchButton.addTarget(self, action:"switchValueChange:", forControlEvents: UIControlEvents.ValueChanged)
+    
+    webView = UIWebView(frame: view.bounds)
+    let url = NSURL(string: "http://iwxy.cc/mqt")
+    webView.loadRequest(NSURLRequest(URL: url!))
+    webView.scrollView.bounces = false
+    webView.delegate = self
+    view.addSubview(webView)
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  //MARK:- UITableView
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 4
-  }
-  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    var cell = tableView.dequeueReusableCellWithIdentifier(Identifier) as? UITableViewCell
-    if cell == nil {
-      cell = NSBundle.mainBundle().loadNibNamed("SkipCheckInSettingListCell", owner: nil, options: nil).last as! SkipCheckInSettingListCell
+  func switchValueChange(sender: UISwitch) {
+    if switchButton.on {
+      switchStatus.text = "开启"
+    }else {
+      switchStatus.text = "关闭"
     }
-    return cell!
   }
-  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    
+  
+  func webViewDidStartLoad(webView: UIWebView) {
+    ZKJSTool.showLoading()
+  }
+  func webViewDidFinishLoad(webView: UIWebView) {
+    ZKJSTool.hideHUD()
   }
 }
