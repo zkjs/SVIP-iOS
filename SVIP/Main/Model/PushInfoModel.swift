33470b546242dbe09b8d4ef5af937450899d2a95
Hanton <hanton@zkjinshi.com>
2015-12-23 15:30:20 +0800

    加Log

--- SVIP/Main/Model/PushInfoModel.swift
+++ SVIP/Main/Model/PushInfoModel.swift
@@ -9,23 +9,33 @@
 import UIKit
 
 class PushInfoModel: NSObject {
+  
   var desc: String!
   var iconbaseurl: String!
   var iconfilename: String!
   var shopid: String!
   var title: String!
   var orderNo: String!
+  
   override init() {
     super.init()
   }
+  
   init(dic: NSDictionary) {
-    
-    desc = dic["desc"] as! String
-    iconbaseurl = dic["iconbaseurl"] as! String
-    iconbaseurl = dic["iconbaseurl"] as? String
-    iconfilename = dic["iconfilename"] as? String
-    shopid = dic["shopid"] as? String
-    title = dic["title"] as? String
-    orderNo = dic["orderNo"] as? String
+    if let shopid = dic["shopid"] as? String {
+      self.shopid = shopid
+    } else {
+      ZKJSTool.showMsg("shopid无值\(dic)")
+    }
+    if let orderNo = dic["orderNo"] as? String {
+      self.orderNo = orderNo
+    } else {
+      ZKJSTool.showMsg("orderNo无值\(dic)")
+    }
+    desc = dic["desc"] as? String ?? ""
+    iconbaseurl = dic["iconbaseurl"] as? String ?? ""
+    iconfilename = dic["iconfilename"] as? String ?? ""
+    title = dic["title"] as? String ?? ""
   }
+  
 }
