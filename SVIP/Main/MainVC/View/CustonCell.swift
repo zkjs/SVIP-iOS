bd9178033f891be152d43e70f0f00fb49099cf15
Hanton <hanton@zkjinshi.com>
2016-01-05 16:31:43 +0800

    初始化隐藏蓝牙提示

--- SVIP/Main/MainVC/View/CustonCell.swift
+++ SVIP/Main/MainVC/View/CustonCell.swift
@@ -27,6 +27,8 @@ class CustonCell: UITableViewCell {
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
+    
+    bluetoolView.hidden = true
   }
   
   class func reuseIdentifier() -> String {
