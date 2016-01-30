4f9b91033ff156bd8d1ba526306bc6ca73e23c58
Hanton <hanton@zkjinshi.com>
2016-01-30 17:20:21 +0800

    关闭在线支付选项

--- SVIP/Book/Controller/HotelOrderTVC.swift
+++ SVIP/Book/Controller/HotelOrderTVC.swift
@@ -172,6 +172,11 @@ class HotelOrderTVC: UITableViewController,UITextFieldDelegate {
   func choosePayStatus() {
     let alertView = UIAlertController(title: "选择订单状态", message: "", preferredStyle: .ActionSheet)
     for index in 1..<paytypeArray.count {
+      // 关闭在线支付选项
+      if index == 1 {
+        continue
+      }
+      
       alertView.addAction(UIAlertAction(title: paytypeArray[index], style: .Default, handler: { [unowned self] (action: UIAlertAction!) -> Void in
         self.paymentLabel.text = self.paytypeArray[index]
         // 更新订单
