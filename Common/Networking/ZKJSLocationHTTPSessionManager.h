a021781a835995fe655992bdebc56512c50417f1
AlexBang <1324762689@qq.com>
2016-02-23 19:17:02 +0800

    增加室内区域通知API

--- Common/Networking/ZKJSLocationHTTPSessionManager.h
+++ Common/Networking/ZKJSLocationHTTPSessionManager.h
@@ -11,4 +11,7 @@
 @interface ZKJSLocationHTTPSessionManager : AFHTTPSessionManager
 #pragma mark - 单例
 + (instancetype)sharedInstance;
+
+#pragma mark - 推送/更新室内位置
+- (void)regionalPositionChangeNoticeWithMajor:(NSString *)major locID:(NSString *)locID minior:(NSString *)minior uuid:(NSString *)uuid sensorid:(NSString *)sensorid timestamp:(NSNumber*)timestamp success:(void (^)(NSURLSessionDataTask *task, id responseObject))success failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
 @end
--- Common/Networking/ZKJSLocationHTTPSessionManager.m
+++ Common/Networking/ZKJSLocationHTTPSessionManager.m
@@ -45,4 +45,16 @@
   return [AccountManager sharedInstance].userName;
 }
 
+#pragma mark - 推送/更新室内位置
+- (void)regionalPositionChangeNoticeWithMajor:(NSString *)major locID:(NSString *)locID minior:(NSString *)minior uuid:(NSString *)uuid sensorid:(NSString *)sensorid timestamp:(NSNumber*)timestamp success:(void (^)(NSURLSessionDataTask *task, id responseObject))success failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure {
+  NSDictionary * dic = @{@"major":major,@"minior":minior,@"locid":locID,@"uuid":uuid,@"sensorid":sensorid,@"timestamp":timestamp};
+  [self POST:@"application/json" parameters:dic success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+    //    NSLog(@"%@", responseObject);
+    success(task, responseObject);
+  } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+    NSLog(@"%@", [error description]);
+    failure(task, error);
+  }];
+}
+
 @end
