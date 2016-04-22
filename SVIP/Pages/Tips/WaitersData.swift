2347f68640effc63c52f926acfd3c28cf9949652
qinyejun <qinyejun2005@gmail.com>
2016-04-22 17:36:26 +0800

    1.waiter popout viewcontroller & animation 2.payment success viewcontroller

--- SVIP/Pages/Tips/WaitersData.swift
+++ SVIP/Pages/Tips/WaitersData.swift
@@ -15,10 +15,34 @@ struct Waiter {
 }
 
 struct WaitersData {
+  private(set) var currentIndex = 0
+  
   static let allWaiters = [
     Waiter(userid: "c_user_01", name: "徐伟", avatar: "uploads/users/c_8bef4a7faa55bdaa_1460950874292.jpg"),
     Waiter(userid: "c_user_01", name: "覃业俊", avatar: "uploads/users/c_87db4f52b941071b_1461228041883.jpg"),
     Waiter(userid: "c_user_01", name: "林琳", avatar: "uploads/users/c_58b7444ba221520c_1461238139584.jpg"),
     Waiter(userid: "c_user_01", name: "梁颖", avatar: "uploads/users/c_727948d0a899b53b_1460173623718.jpg")
   ]
+  
+  mutating func next() -> Waiter? {
+    if WaitersData.allWaiters.count < 1 {
+      return nil
+    }
+    if currentIndex < WaitersData.allWaiters.count {
+      currentIndex += 1
+    } else {
+      currentIndex = 1
+    }
+    return WaitersData.allWaiters[currentIndex - 1]
+  }
+  
+  func currentWaiter() -> Waiter {
+    var index = currentIndex - 1
+    if currentIndex < 0 {
+      index = 0
+    } else if currentIndex >= WaitersData.allWaiters.count {
+      index = WaitersData.allWaiters.count - 1
+    }
+    return WaitersData.allWaiters[index]
+  }
 }
